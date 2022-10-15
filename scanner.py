d03ab6ca9348313b369b8ff0db5d4c3f6bb60cfa
manojvd <113596652+manojvd@users.noreply.github.com>
2022-10-15 22:39:45 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -30,6 +30,26 @@ class ResumeScanner(object):
         return self.__details
 
     def __get_basic_details(self):
+        name       = utils.extract_name(self.__nlp, matcher=self.__matcher)
+        email      = utils.extract_email(self.__text)
+        mobile     = utils.extract_mobile_number(self.__text)
+        skills     = utils.extract_skills(self.__nlp, self.__noun_chunks)
+        edu        = utils.extract_education([sent.string.strip() for sent in self.__nlp.sents])
+        experience = utils.extract_experience(self.__text)
+        entities   = utils.extract_entity_sections(self.__text_raw)
+        self.__details['name'] = name
+        self.__details['email'] = email
+        self.__details['mobile_number'] = mobile
+        self.__details['skills'] = skills
+        # self.__details['education'] = entities['education']
+        self.__details['education'] = edu
+        self.__details['experience'] = experience
+        try:
+            self.__details['competencies'] = utils.extract_competencies(self.__text_raw, entities['experience'])
+            self.__details['measurable_results'] = utils.extract_measurable_results(self.__text_raw, entities['experience'])
+        except KeyError:
+            self.__details['competencies'] = []
+            self.__details['measurable_results'] = []
         return
 
 def resume_result_wrapper(resume):
