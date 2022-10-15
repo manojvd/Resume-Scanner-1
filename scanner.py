a19e8f3dd34a1394ce3edd0fc6889eabb6465070
Nireeksha D Rai <76628362+Nireekshadrai@users.noreply.github.com>
2022-10-15 21:13:51 +0530

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,53 @@
+import os
+from . import utils
+import spacy
+import pprint
+from spacy.matcher import Matcher
+import multiprocessing as mp
+
+class ResumeScanner(object):
+    def __init__(self, resume):
+        nlp = spacy.load('en_core_web_sm')
+        self.__matcher = Matcher(nlp.vocab)
+        self.__details = {
+            'name'              : None,
+            'email'             : None,
+            'mobile_number'     : None,
+            'skills'            : None,
+            'education'         : None,
+            'experience'        : None,
+            'competencies'      : None,
+            'measurable_results': None
+        }
+        self.__resume      = resume
+        self.__text_raw    = utils.extract_text(self.__resume, os.path.splitext(self.__resume)[1])
+        self.__text        = ' '.join(self.__text_raw.split())
+        self.__nlp         = nlp(self.__text)
+        self.__noun_chunks = list(self.__nlp.noun_chunks)
+        self.__get_basic_details()
+
+    def get_extracted_data(self):
+        return self.__details
+
+    def __get_basic_details(self):
+        return
+
+def resume_result_wrapper(resume):
+        parser = ResumeParser(resume)
+        return parser.get_extracted_data()
+
+if __name__ == '__main__':
+    pool = mp.Pool(mp.cpu_count())
+
+    resumes = []
+    data = []
+    for root, directories, filenames in os.walk('resumes'):
+        for filename in filenames:
+            file = os.path.join(root, filename)
+            resumes.append(file)
+
+    results = [pool.apply_async(resume_result_wrapper, args=(x,)) for x in resumes]
+
+    results = [p.get() for p in results]
+
+    pprint.pprint(results)
